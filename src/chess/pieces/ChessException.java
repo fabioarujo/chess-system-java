6284078f1a40dbdf0811ef1d826504d052bdf6f8
Fábio Araújo <fabioaraujo1602@gmail.com>
2021-03-03 15:43:31 -0300

    ChessException e ChessPOsition

---
+++ src/chess/pieces/ChessException.java
@@ -0,0 +1,13 @@
+package chess.pieces;
+
+public class ChessException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ChessException(String msg) {
+		super(msg);
+	}
+	
+	
+	
+}
